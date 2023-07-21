5d785cee0ae2ef5953ff4afde469f3fea92023a9
Mamou Skn <mamou.test@gmail.com>
2023-07-21 14:29:57 +0200

    Resolution quete

---
+++ information.js
@@ -0,0 +1,14 @@
+const firstName = "leM";
+const campus = "Paris";
+
+// function displayPassions() {
+//   console.log(`My passions are :`);
+//   for (let i = 0; i < passions.length; i += 1) {
+//     console.log(`- ${passions[i]}`);
+//   }
+// }
+
+module.exports = {
+  firstName: firstName,
+  campus: campus
+};
